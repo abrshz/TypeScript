5df9cc13007a06c2f28d2e9aa82cdcd02e213dba
abrshz <trusewynur@gmail.com>
2025-12-15 12:00:14 +0300

    Practise with null and undefine datatypes

---
+++ Section2/index.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// Writing String variables 
+let userName = "John Doe Abraham Haile";
+console.log(userName);
+//# sourceMappingURL=index.js.map
---
+++ Section2/index.ts
@@ -0,0 +1,21 @@
+// Writing String variables 
+let userName: string = "John Doe";
+// userName = '1';
+console.log(`How are you ${userName}?`)
+
+
+
+// Writing null datatype 
+let userSelectedOption:null = null;
+
+console.log(userSelectedOption);
+
+console.log(typeof userName);
+
+// Writing undefine datatype
+
+let name:undefined;
+
+console.log(name);
+
+console.log(typeof name);
