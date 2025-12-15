267e69ec6c34c8534359b0eb4a83d84e45e4e842
abrshz <trusewynur@gmail.com>
2025-12-15 13:08:34 +0300

    Updated the index.js file

--- Section2/index.js
+++ Section2/index.js
@@ -1,6 +1,15 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 // Writing String variables 
-let userName = "John Doe Abraham Haile";
-console.log(userName);
+let userName = "John Doe";
+// userName = '1';
+console.log(`How are you ${userName}?`);
+// Writing null datatype 
+let userSelectedOption = null;
+console.log(userSelectedOption);
+console.log(typeof userName);
+// Writing undefine datatype
+let name;
+console.log(name);
+console.log(typeof name);
 //# sourceMappingURL=index.js.map
