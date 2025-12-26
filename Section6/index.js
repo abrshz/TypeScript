b5e7506cd4e56bb15ba6bb09f9ab7b8393212a8e
abrshz <trusewynur@gmail.com>
2025-12-26 13:09:56 +0300

    Inttroduction about fucntion

---
+++ Section6/index.js
@@ -0,0 +1,47 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// Named function 
+function intro(name, age) {
+    return `My name is ${name} and I am ${age} years old.`;
+}
+// Function expression
+const intro2 = function (name, age) {
+    return `My name is ${name} and I am ${age} years old.`;
+};
+// Arrow function 
+const intro3 = (name, age) => {
+    return `My name is ${name} and I am ${age} years old.`;
+};
+// 
+function intros(name, age, country) {
+    if (country) {
+        return `My name is ${name} and I am ${age} years old. I live in ${country}.`;
+    }
+    return `My name is ${name} and I am ${age} years old.`;
+}
+console.log(intros("Mike", 32, "Eritrea"));
+console.log(intros('John', 32));
+console.log(intros('Alice', 28, 'Canada'));
+// 
+// enum AgeUnit {
+//     Years = 'years',
+//     Months = 'months',
+// };
+const AgeUnit = {
+    Years: 'years',
+    Months: 'months',
+};
+const person = {
+    name: 'Scott',
+    age: 30,
+    ageUnit: AgeUnit.Years,
+};
+function convertAgeToMonths(person) {
+    if (person.ageUnit === AgeUnit.Years) {
+        person.age = person.age * 12;
+        person.ageUnit = AgeUnit.Months;
+    }
+    return person;
+}
+console.log(convertAgeToMonths(person));
+//# sourceMappingURL=index.js.map
---
+++ Section6/index.ts
@@ -0,0 +1,69 @@
+// Named function 
+function intro(name: string, age:number): string{
+    return `My name is ${name} and I am ${age} years old.`
+}
+
+// Function expression
+const intro2 = function(name: string, age: number): string{
+     return `My name is ${name} and I am ${age} years old.`
+}
+
+// Arrow function 
+const intro3 = (name: string, age: number): string | number => {
+     return `My name is ${name} and I am ${age} years old.`
+}
+
+
+// 
+function intros(name: string, age:number, country?:string): string{
+    if(country){
+    return `My name is ${name} and I am ${age} years old. I live in ${country}.`
+
+    }
+    return `My name is ${name} and I am ${age} years old.`
+}
+
+console.log(intros("Mike", 32 , "Eritrea"));
+
+console.log(intros('John', 32));
+console.log(intros('Alice', 28, 'Canada')); 
+
+
+// 
+
+// enum AgeUnit {
+//     Years = 'years',
+//     Months = 'months',
+// };
+
+ const AgeUnit = {
+    Years: 'years',
+    Months: 'months',
+} as const;
+
+// Create a type from the object values
+type AgeUnit = (typeof AgeUnit)[keyof typeof AgeUnit];
+
+type Person ={ 
+    name: string;
+    age: number;
+    ageUnit: AgeUnit;
+};
+
+const person: Person = {
+    name: 'Scott',
+    age: 30,
+    ageUnit: AgeUnit.Years,
+}
+
+function convertAgeToMonths(person: Person): Person {
+    if (person.ageUnit === AgeUnit.Years){
+        person.age = person.age * 12;
+        person.ageUnit = AgeUnit.Months;
+    }
+    return person;
+}
+
+console.log(convertAgeToMonths(person));
+
+
